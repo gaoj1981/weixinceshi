033edee4271a51865da37272c153beabaf28b6c5
gaoj1981 <www.woniu8.com@163.com>
2018-04-13 22:08:32 +0800

    'yutest'

--- ceshi/pages/my/test/mytest.js
+++ ceshi/pages/my/test/mytest.js
@@ -19,19 +19,20 @@ Page({
     testList: [],
     curPg: 1,
     isHavePage: true,
+    col0test1:0,
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    this.setData({col0test1:options.col0test1});
     var _this = this;
     wx.request({
       url: 'https://manage.5dwo.com/out/woniu8/getMyTestList.srv',
       data: {
-        col0test1: 1,
-        fetchPage: _this.data.curPg,
+        col0test1: this.data.col0test1,
+        fetchPage: this.data.curPg,
         limit: 20,
         curOpenId: app.getCurOpenId()
       },
@@ -81,6 +82,50 @@ Page({
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
+    if (this.data.isHavePage) {
+
+      var _this = this;
+      wx.showLoading({
+        title: '',
+        success: function () {
+          var nextPg = _this.data.curPg + 1;
+          wx.request({
+            url: 'https://manage.5dwo.com/out/woniu8/getMyTestList.srv',
+            data: {
+              curOpenId: app.getCurOpenId(),
+              fetchPage: nextPg,
+              col0test1: _this.data.col0test1,
+              limit: 20,
+            },
+            success: function (res) {
+              if (res.data.resObj.length > 0) {
+                var testList = _this.data.testList;
+                testList = testList.concat(res.data.resObj);
+                _this.setData({ testList: testList, curPg: nextPg });
+                wx.hideLoading();
+              } else {
+                wx.hideLoading();
+                wx.showToast({
+                  title: '“蜗”是有底线的',
+                  icon: 'none',
+                  duration: 1000
+                })
+                _this.setData({ isHavePage: false, curPg: nextPg });
+              }
+            }
+          })
+
+        }
+      })
+
+    } else {
+      wx.showToast({
+        title: '“蜗”是有底线的',
+        icon: 'none',
+        duration: 1000
+      })
+    }
+
   
   },
 
